4f0151039634a185f12b8cf8e3f69c659539c448
Tutos Rive <108991712+tutosrive@users.noreply.github.com>
2025-03-26 18:17:11 -0500

    Remove inconsistent atributtes from `Player` class

--- web/src/models/Player.model.js
+++ web/src/models/Player.model.js
@@ -5,30 +5,15 @@ export default class Player {
    */
   #id;
   /**
-   * **Player** name
-   * @type {String}
-   */
-  #name;
-  /**
    * **Player** alias into game
    * @type {String}
    */
   #nickname;
   /**
-   * **Player** total score
-   * @type {Number}
-   */
-  #score;
-  /**
    * **Player** Score actual level
    * @type {Number}
    */
   #score_level;
-  /**
-   * Code of country **Player**. Example: "CO"
-   * @type {String} Max length: 2 chars
-   */
-  #country;
   /**
    * Ships list
    * @type {Array<Ship>}
@@ -38,7 +23,6 @@ export default class Player {
   constructor() {
     if (this.constructor === Player) throw new Error('This is a ABSTRACT class, not allow constructor');
   }
-
   /**
    * This method, spread the Event "PlayerPlay()" and the required data send via "Event"
    */
