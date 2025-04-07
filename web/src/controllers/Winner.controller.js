e63ee3b59926a2f8a0bfa414b764225a34681c31
nat-esp <natyespinosa2006@gmail.com>
2025-04-07 06:26:22 -0500

    Winner controller update

--- web/src/controllers/Winner.controller.js
+++ web/src/controllers/Winner.controller.js
@@ -1,11 +1,27 @@
-// Elementos del DOM
-const overlay = document.querySelector(".popup-overlay");
-const popup = document.querySelector(".popup-container");
-const btnPlayAgain = document.querySelector(".play-again");
-const btnExitGame = document.querySelector(".exit-game");
+let timeout;
+
+export async function winner() {
+  const container = document.querySelector("#main");
+  const res = await fetch("./html/Winner.html");
+  const html = await res.text();
+  container.innerHTML += html;
+  load_winner();
+}
+
+function load_winner() {
+  // Elementos del DOM
+  const overlay = document.querySelector(".popup-overlay");
+  const popup = document.querySelector(".popup-container");
+  const btnPlayAgain = document.querySelector(".play-again");
+  const btnExitGame = document.querySelector(".exit-game");
+  showPopup(overlay, popup);
+  // Event listeners
+  btnPlayAgain.addEventListener("click", restartGame);
+  btnExitGame.addEventListener("click", exitGame);
+}
 
 // Mostrar el popup
-function showPopup() {
+function showPopup(overlay, popup) {
   overlay.style.display = "flex";
   popup.style.animation = "popIn 0.3s ease-out";
 }
@@ -18,14 +34,18 @@ function hidePopup() {
 // Reiniciar el juego
 function restartGame() {
   hidePopup();
-  alert("Reiniciando juego...");
+  load_home
 }
 
 // Salir del juego
 function exitGame() {
-  alert("Saliendo del juego...");
+  Toast.show({ message: "Returning to the beginning" });
+  load_home();
 }
 
-// Event listeners
-btnPlayAgain.addEventListener("click", restartGame);
-btnExitGame.addEventListener("click", exitGame);
+function load_home() {
+  timeout = setTimeout(() => {
+    location.hash = "#home";
+    timeout = null; 
+  }, 3000); 
+}
